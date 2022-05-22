f514612958c9dcec7c408e4536826240bba42f1c
Kairyu <kairyu.bonvivant@gmail.com>
2022-05-22 15:23:50 +0900

    Add 注文機能 routes

---
+++ docker/app/Foodhub/app/Http/Controllers/Auth/OrderItemController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\StoreOrder;
+use App\Models\OrderItem;
+use App\Models\User;
+use App\Models\Item;
+
+class OrderItemController extends Controller
+{
+    //
+}
---
+++ docker/app/Foodhub/app/Http/Controllers/Auth/StoreOrderController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\StoreOrder;
+use App\Models\User;
+use App\Models\Item;
+
+class StoreOrderController extends Controller
+{
+    //
+}

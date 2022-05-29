88be402bb95cac558537dba3fe540f7382ca2689
Kairyu <kairyu.bonvivant@gmail.com>
2022-05-29 19:16:01 +0900

    [Add メール機能]

--- docker/app/Foodhub/app/Http/Controllers/Admin/StoreController.php
+++ docker/app/Foodhub/app/Http/Controllers/Admin/StoreController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\WelcomeMail;
+use App\Mail\WarningMail;
 use App\Models\Store;
 
 class StoreController extends Controller
@@ -45,6 +48,16 @@ class StoreController extends Controller
         unset($params['_token']);
         //保存
         $store->fill($params)->save();
+
+        $name = $store->name;
+        $email = $store->email;
+        $admin_email = config('mail.mailers.smtp.username');
+        $admin_tel = '0120-000-000';
+        if ($store->is_deleted == 1) {
+            Mail::send(new WelcomeMail($name, $email, $admin_email, $admin_tel));
+        } elseif ($store->is_deleted == 0) {
+            Mail::send(new WarningMail($name, $email, $admin_email, $admin_tel));
+        }
     } catch (\Exception $e) {
         return back()->with('msg-danger', 'ステータスの変更に失敗しました');
     }
---
+++ docker/app/Foodhub/app/Mail/WelcomeMail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class WelcomeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $name;
+    public $email;
+    public $admin_email;
+    public $admin_tel;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($name, $email, $admin_email, $admin_tel)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->admin_email = $admin_email;
+        $this->admin_tel = $admin_tel;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->to($this->email)
+        ->subject('ご登録が完了しました。')
+        ->view('mail.welcome_mail')
+        ->with(['name'=> $this->name, 'admin_email'=>$this->admin_email, 'admin_tel'=>$this->admin_tel]);
+    }
+}
